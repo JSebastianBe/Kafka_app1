4d473fa1ac48da60d5624c71ea736aabd4f25097
JSebastianBe <sebastianbm97@gmail.com>
2024-09-16 15:21:31 -0500

    Subida inicial

---
+++ src/main/java/com/mycompany/kafka_app1/Kafka_app1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.kafka_app1;
+
+/**
+ *
+ * @author jusebema
+ */
+public class Kafka_app1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
